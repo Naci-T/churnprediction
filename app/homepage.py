de00930669bd00ebfeb6a6d97860e66b0115f5b5
Naci-T <tanriovennaci@gmail.com>
2023-02-28 15:26:29 +0100

    app added

---
+++ app/homepage.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+def homepage():
+    st.markdown("# Churn Halt ")
+    st.sidebar.markdown("# Churn Halt")
+
+def predict():
+    st.markdown("# Churn prediction ")
+    st.sidebar.markdown("# Churn prediction ")
+
+def tableau():
+    st.markdown("# Tableau ")
+    st.sidebar.markdown("# Tableau ")
+
+page_names_to_funcs = {
+    "Main Page": homepage,
+    "Page 2": predict,
+    "Page 3": tableau,
+}
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://img.freepik.com/free-vector/white-color-background-gradient-abstract-modern_343694-2130.jpg?w=826&t=st=1677495167~exp=1677495767~hmac=8695c6bbf17e5b28ac9aa25b002e83829f37f83326cf9e7b3a1184b76a44e46a");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url() 
+
+st.title("Welcome to Churn Halt!")
+
+st.write("This website is designed to help banks predict which customers are likely to churn, and provide valuable insights on how to retain them. With Churn Halt, you can access our powerful machine learning models to analyze customer behavior and other key factors to determine the likelihood of churn.")
+st.write("Our user-friendly interface makes it easy for you to make predictions with just a few clicks, and our advanced algorithms will provide you with accurate insights and recommendations on how to keep your customers engaged and satisfied. With Churn Halt, you can reduce customer churn, improve customer loyalty, and ultimately increase your revenue.")
+st.write("In addition, we've created a separate page where you can visualize the data using Tableau Public. Here, you can explore the patterns and trends in customer behavior and gain deeper insights into the factors that drive customer churn.")
+st.write("So, whether you're a small credit union or a large multinational bank, Churn Halt can help you take control of your customer retention strategy and improve your bottom line. Let's get started!")
+# selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+# page_names_to_funcs[selected_page]()
---
+++ app/pages/predict.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import xgboost as xgb
+#import numpy as np 
+
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://img.freepik.com/free-vector/white-background-with-hexagonal-line-pattern-design_1017-28442.jpg?w=826&t=st=1677494980~exp=1677495580~hmac=2214c1492d75747aaf7f3a1a119c7aedba880f800dd10c11b9b2d2f9f1b03457");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url() 
+
+st.title("Please enter the details to see if the customer will churn or not")
+
+# User input
+
+total_trans_ct = st.number_input('Enter the total transaction count', min_value=0, max_value=200)
+
+total_trans_amt = st.number_input('Enter the total transaction amount', min_value=0, max_value=30000) # 20 = High probability to churn, 80 = Low probability to churn
+
+total_revolving_bal=st.number_input('Total revolving balance', min_value=0, max_value=4000, key="total_revolving_bal")
+
+tac_Q4_Q1=st.number_input('Total amount change from quarter 4 to quarter 1', min_value=0.0, max_value=4.0, step=0.05)
+
+tcc_Q4_Q1= st.number_input('Total ct change from quarter 4 to quarter 1', min_value=0.0, max_value=4.0, step=0.05)
+
+trc= st.select_slider('Number of products at the bank:', [1,2,3,4,5,6], key="trc")
+
+months_inactive= st.select_slider("Months of inactivity in previous year", [0,1,2,3,4,5,6], key="months_inactive")
+
+contacts_count= st.select_slider("How many times contacted in previous year:", [0,1,2,3,4,5,6], key="contacts_count")
+
+age= st.slider("What's the customer's age:", 18,100, key="age")
+
+credit=st.slider("Credit limit:", 1000, 40000, key="credit")
+
+mob= st.slider("Months on book:", 12, 60, key="mob")
+
+# Loading the model
+model_xgb = xgb = xgb.XGBClassifier(
+    learning_rate=0.05,
+    max_depth=4,
+    min_child_weight=1,
+    n_estimators=500,
+    subsample=0.4331229815476772,
+    colsample_bytree=0.5242723167060505,
+    reg_lambda=7.049263425549493e-06,
+    reg_alpha=0.015123845878697514,
+)
+model_xgb.load_model("./model/xgbmodel.bin")
+
+# Make prediction: 
+
+user_input = pd.DataFrame([[total_trans_ct, total_trans_amt, total_revolving_bal, tac_Q4_Q1, tcc_Q4_Q1, trc, months_inactive, contacts_count, age, credit, mob]], columns=["Total_Trans_Ct","Total_Trans_Amt", "Total_Revolving_Bal", "Total_Amt_Chng_Q4_Q1", "Total_Ct_Chng_Q4_Q1", "Total_Relationship_Count", "Months_Inactive_12_mon", "Contacts_Count_12_mon", "Customer_Age", "Credit_Limit", "Months_on_book"])
+prediction = model_xgb.predict_proba(user_input)
+
+if st.button("Predict"):
+    churn_prob = round(prediction[0][1] * 100, 2)
+    if churn_prob > 50:
+        st.error(f"This customer has a {churn_prob}% chance to churn")
+    else:
+        st.success(f"This customer has a {churn_prob}% chance to churn")
